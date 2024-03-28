3ee607374ffa34e66d0cf401d9b41f74751e1123
rudwheel <rudy.anne.pro@gmail.com>
2024-03-28 15:36:24 +0100

    Appel de RandomList et affichage de la liste traitée
    - main.ts:
    
    Appel de RandomList et affichage de la liste traitée

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
-import { generateRandomList} from './core';
+import { generateRandomList, processList } from "./core";
 
 // Définit la taille de la liste à générer
 const listSize = 10;
 
-
 // Génère une liste de nombres aléatoires de taille listSize
-const randomList = generateRandomList(listSize); 
+const randomList = generateRandomList(listSize);
 
 // Affiche la liste de nombres aléatoires générée
 console.log("Liste de nombres aléatoires : ", randomList);
+
+// Traite la liste de nombres aléatoires avec la fonction processList
+const results = processList(randomList);
+
+// Affiche les résultats du traitement de la liste
+console.log("Résultats du traitement de la liste : ", results);
