8755cef102f30105b43b22f571aba4bd9b2e37b7
rudwheel <rudy.anne.pro@gmail.com>
2024-03-28 13:47:02 +0100

    analyseNumber: divisible par 3
    core.ts :
    
    analyseNumber :
    
    Cas nombre divisible par 3
    
    -core.spec.ts :
    Ajout des tests associés.

--- src/core.spec.ts
+++ src/core.spec.ts
@@ -6,4 +6,10 @@ describe("analyzeNumber", () => {
     expect(analyzeNumber(30)).toBe("Gestform");
     expect(analyzeNumber(45)).toBe("Gestform");
   });
+  
+  it('should return "Geste" when the number is divisible by 3 but not by 5', () => {
+    expect(analyzeNumber(3)).toBe("Geste");
+    expect(analyzeNumber(6)).toBe("Geste");
+    expect(analyzeNumber(9)).toBe("Geste");
+  });
 });
--- src/core.ts
+++ src/core.ts
@@ -3,6 +3,10 @@ export function analyzeNumber(inputNumber: number): string {
   if (inputNumber % 3 === 0 && inputNumber % 5 === 0) {
     return "Gestform";
   }
+  // Si le nombre est un multiple de 3, retourne "Geste"
+  else if (inputNumber % 3 === 0) {
+    return "Geste";
+  }
   // Si le nombre n'est pas un multiple de 3 ou 5, retourne le nombre sous forme de chaîne de caractères
   else {
     return inputNumber.toString();
