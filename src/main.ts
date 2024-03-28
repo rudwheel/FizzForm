d016b363db27af4bb395458aea727f4a96b6d34b
rudwheel <rudy.anne.pro@gmail.com>
2024-03-28 15:06:31 +0100

    Ajout appel et affichage de la liste aleatoire dans main.ts
    -main.ts:
    
    Ajout appel et affichage de la liste aleatoire générée.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
+import { generateRandomList} from './core';
 
+// Définit la taille de la liste à générer
+const listSize = 10;
+
+
+// Génère une liste de nombres aléatoires de taille listSize
+const randomList = generateRandomList(listSize); 
+
+// Affiche la liste de nombres aléatoires générée
+console.log("Liste de nombres aléatoires : ", randomList);
